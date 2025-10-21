244b71c327e076540806d3e2474fbbb3e63d5b1d
elisevgeniy <elisevgeniy@gmail.com>
2025-10-21 22:54:34 +0300

    feat: валидация формы авторизации

--- ContractSystem.WebApp/ContractSystem.WebApp/Components/Models/LoginModel.cs
+++ ContractSystem.WebApp/ContractSystem.WebApp/Components/Models/LoginModel.cs
@@ -1,8 +1,12 @@
-﻿namespace ContractSystem.WebApp.Components.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContractSystem.WebApp.Components.Models
 {
     public class LoginModel
     {
+        [Required(ErrorMessage ="Поле обязательно к заполнению")]
         public string Login { get; set; }
+        [Required(ErrorMessage = "Поле обязательно к заполнению")]
         public string Password { get; set; }
     }
 }
